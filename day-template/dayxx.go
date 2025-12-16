6ae0ebcff8496f7641eff63e17d5bb30d54b9945
Michael Jagroep <michael.jagroep@gmail.com>
2025-12-16 15:38:22 +0000

    add day template

---
+++ day-template/dayxx.go
@@ -0,0 +1,18 @@
+package dayxx
+
+import (
+	"errors"
+	"fmt"
+	"m0k0/advent-2025/common"
+)
+
+func Solve(advent *common.AdventSetup) (string, error) {
+
+	inputFile, err := advent.OpenInput()
+	if err != nil {
+		return "", fmt.Errorf("day %d error: \n%w", advent.Day, err)
+	}
+	defer inputFile.Close()
+
+	return "", errors.New("no solution available")
+}
