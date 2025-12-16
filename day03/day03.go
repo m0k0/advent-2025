da3abe237472d5b23f23fdfad5440a059d93f2ee
Michael Jagroep <michael.jagroep@gmail.com>
2025-12-16 11:53:18 +0000

    fix typo

--- day03/day03.go
+++ day03/day03.go
@@ -13,7 +13,7 @@ func Solve(advent *common.AdventSetup) (string, error) {
 
 	inputFile, err := advent.OpenInput()
 	if err != nil {
-		return "", fmt.Errorf("day 2 error: \n%w", err)
+		return "", fmt.Errorf("day 3 error: \n%w", err)
 	}
 	defer inputFile.Close()
 
--- day04/day04.go
+++ day04/day04.go
@@ -18,7 +18,7 @@ func Solve(advent *common.AdventSetup) (string, error) {
 
 	inputFile, err := advent.OpenInput()
 	if err != nil {
-		return "", fmt.Errorf("day 2 error: \n%w", err)
+		return "", fmt.Errorf("day 4 error: \n%w", err)
 	}
 	defer inputFile.Close()
 
